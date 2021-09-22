f5323e81414b198011278eddf02ddd12d8b31c21
snrnieto <49009866+snrnieto@users.noreply.github.com>
2021-09-21 20:21:17 -0500

    funcion eliminar usuario al desconectarse

--- index.js
+++ index.js
@@ -7,28 +7,48 @@ var port = process.env.PORT || 5000
 var peers=[];
 
 app.get('/',(req,res) => {
-    res.send('Hola mundo')
+    res.send('Hola mundo') 
 })
 
 const io = require("socket.io")(server, {
-    cors: {
-        origin: "*",
+    cors: { 
+        origin: "*", 
     }
 })
 
 io.on("connection", (socket) => {
-	// console.log("Usuario conectado "+socket.id)
-	// socket.emit("me", socket.id);
 
-	socket.on('join-room', (userId) => {
-		console.log(userId)
-		peers.push(userId)
+	var socket_id = socket.id;
+
+	socket.on('new-user', (userId) => {
+		console.log("Usuario conectado: "+userId)
+
+		var new_peer = new Object();
+
+		new_peer.socket_id = socket_id;
+		new_peer.peer = userId;
+
+		peers.push(new_peer)
 		console.log("Usuarios: ",peers)
 
-		io.emit("new-user",peers)
+		io.emit("peers",peers)
 	})
+
+	socket.on('disconnect', () => {
+		
+		peers = deleteWithId(peers,socket.id) 
+		console.log("Usuario desconectado: "+socket.id)
+		console.log("Usuarios: ",peers)
+
+		io.emit("user-disconnected",socket.id)
+	}) 
 	
 });
 
 
 server.listen(port, () => console.log("server is running on port "+port))
+ 
+
+function deleteWithId(list,id){
+	return list.filter(function(el) { return el.socket_id != id; }); 
+}
